912d8c9adc13e3bc25af1eb5a0a61fb695788dea
Gritier <uhgoci@gmail.com>
2023-12-02 12:37:19 +0100

    Puzzle 2 completed

--- Day1/Puzzle2/main.py
+++ Day1/Puzzle2/main.py
@@ -17,26 +17,15 @@ numbers = {
 
 calibration_values = []
 
+with open("input.txt", "r") as puzzle:
+    input = puzzle.read().splitlines()
 
-def replace_textual(string):
-    if len(string) == 3:
-        for number in numbers.keys():
-            string = string.replace(number, str(numbers[number]))
-    elif len(string) == 4:
-        for i in range(2):
-            if string[i:i+3] in numbers.keys():
-                string = + \
-                    string[i:i+3].replace(string[i:i+3],
-                                          str(numbers[string[i:i+3]]))+string[i+3:]
-        if len(string) > 3:
-            if string in numbers.keys():
-                string = string.replace(string, str(numbers[string]))
+
+def check(string):
+    if string in numbers.keys():
+        return string.replace(string, str(numbers[string]))
     else:
-        for i in range(len(string)-4):
-            for number in numbers.keys():
-                string = string[:i]+string[i:i +
-                                           5].replace(number, str(numbers[number]))+string[i+5:]
-    return string
+        return string
 
 
 def replace_residual(string):
@@ -46,22 +35,60 @@ def replace_residual(string):
     return string
 
 
-with open("input.txt", "r") as puzzle:
-    input = puzzle.read().splitlines()
+def loop_substring(line):
+    for i in range(len(line)-3):
+        for n in range(3, 6):
+            if i+n <= len(line):
+                temp_line = line[:i]+check(line[i:i+n])+line[i+n:]
+                line = line.replace(line, temp_line)
+            else:
+                temp_line = line[0:-3]+check(line[-3:])
+                line = line.replace(line, temp_line)
+
+    return line
+
+
+def analyze_string(string):
+    l = 0
+    r = 0
+    temp = string.replace(string, string+("_"))
+    for i, c in enumerate(temp):
+        if c.isdigit() and l == 0:
+            l = int(c)
+        elif temp[i:i+3] in numbers.keys() and len(temp) > (i+3) and l == 0:
+            l = numbers[temp[i:i+3]]
+        elif temp[i:i+4] in numbers.keys() and len(temp) > (i+4) and l == 0:
+            l = numbers[temp[i:i+4]]
+        elif temp[i:i+5] in numbers.keys() and len(temp) > (i+5) and l == 0:
+            l = numbers[temp[i:i+5]]
+        if i == 0:
+            j = -1
+        else:
+            j = (i*-1)-1
+        if temp[j].isdigit() and r == 0:
+            r = int(temp[j])
+        elif temp[j-3:j] in numbers.keys() and len(temp) > (i+3) and r == 0:
+            r = numbers[temp[j-3:j]]
+        elif temp[j-4:j] in numbers.keys() and len(temp) > (i+4) and r == 0:
+            r = numbers[temp[j-4:j]]
+        elif temp[j-5:j] in numbers.keys() and len(temp) > (i+5) and r == 0:
+            r = numbers[temp[j-5:j]]
+        print(temp[j-5:j])
+    if r == 0:
+        r = l
+    value = int(str(l)+str(r))
+    return value
 
 
 def main():
-    for i in input:
-        line = replace_residual(replace_textual(i))
-        value = int(str(line[0])+str(line[-1]))
+    for line in input:
+        # new_line = replace_residual(loop_substring(line))
+        # value = int(str(new_line[0])+str(new_line[-1]))
+        value = analyze_string(line)
         calibration_values.append(value)
+        print(f'{line} : {value}')
 
-    total = 0
-
-    for v in calibration_values:
-        total += v
-
-    print(total)
+    print(sum(calibration_values))
 
 
 main()
